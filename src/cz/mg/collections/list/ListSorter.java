1b448cfd6e1cc096c27eb8a1ec88ec62757d4f21
Me <gekoncze@seznam.cz>
2021-08-11 20:05:24 +0200

    added sort in place

--- src/cz/mg/collections/list/ListSorter.java
+++ src/cz/mg/collections/list/ListSorter.java
@@ -18,4 +18,18 @@ public class ListSorter {
         Arrays.sort((T[])array.getJavaArray(), comparator);
         return new List<>(array);
     }
+
+    public static <T> void sortInPlace(List<T> list){
+        Array<T> array = new Array<>(list);
+        Arrays.sort(array.getJavaArray());
+        list.clear();
+        list.addCollectionLast(array);
+    }
+
+    public static <T> void sortInPlace(List<T> list, Comparator<T> comparator){
+        Array<T> array = new Array<>(list);
+        Arrays.sort((T[])array.getJavaArray(), comparator);
+        list.clear();
+        list.addCollectionLast(array);
+    }
 }
