6b21b615bda8ebd84fd42dd15c2663bd36911085
Me <gekoncze@seznam.cz>
2021-08-11 17:49:31 +0200

    added list sorter

---
+++ src/cz/mg/collections/list/ListSorter.java
@@ -0,0 +1,21 @@
+package cz.mg.collections.list;
+
+import cz.mg.collections.array.Array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class ListSorter {
+    public static <T> List<T> sort(ReadableList<T> list){
+        Array<T> array = new Array<>(list);
+        Arrays.sort(array.getJavaArray());
+        return new List<>(array);
+    }
+
+    public static <T> List<T> sort(ReadableList<T> list, Comparator<T> comparator){
+        Array<T> array = new Array<>(list);
+        Arrays.sort((T[])array.getJavaArray(), comparator);
+        return new List<>(array);
+    }
+}
